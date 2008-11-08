2bc6902a8df2916a120368082ae28bc045d02d7d
Chris Paiano <chris.paiano@gmail.com>
2008-11-08 05:33:30 +0000

    setup.py and setup_exe.py now have a rudimentary multi-OS friendly structure.
    git-svn-id: http://fofix.googlecode.com/svn/MFH-Mod/trunk@273 48f48816-a574-47fe-9a59-6914b3aef992

--- src/setup.py
+++ src/setup.py
@@ -20,106 +20,19 @@
 # MA  02110-1301, USA.                                              #
 #####################################################################
 
-# Keyboard Hero setup script
-from distutils.core import setup
-import sys, SceneFactory, Version, glob, os
-
-try:
-  import py2exe
-except ImportError:
-  pass
-
-options = {
-  "py2exe": {
-    "dist_dir":  "../dist",
-    "includes":  SceneFactory.scenes,
-    "excludes":  [
-      "glew.gl.apple",
-      "glew.gl.ati",
-      "glew.gl.atix",
-      "glew.gl.hp",
-      "glew.gl.ibm",
-      "glew.gl.ingr",
-      "glew.gl.intel",
-      "glew.gl.ktx",
-      "glew.gl.mesa",
-      "glew.gl.oml",
-      "glew.gl.pgi",
-      "glew.gl.rend",
-      "glew.gl.s3",
-      "glew.gl.sgi",
-      "glew.gl.sgis",
-      "glew.gl.sgix",
-      "glew.gl.sun",
-      "glew.gl.sunx",
-      "glew.gl.threedfx",
-      "glew.gl.win",
-      "ode",
-      "_ssl",
-      "bz2",
-      "email",
-      "calendar",
-      "bisect",
-      "difflib",
-      "doctest",
-      "ftplib",
-      "getpass",
-      "gopherlib",
-      "heapq",
-      "macpath",
-      "macurl2path",
-      "GimpGradientFile",
-      "GimpPaletteFile",
-      "PaletteFile",
-      "macosx",
-    ],
-    "optimize":  2,
-  }
-}
-
-dataFiles = [
-  "default.ttf",
-  "title.ttf",
-  "international.ttf",
-  "key.dae",
-  "note.dae",
-  "cassette.dae",
-  "label.dae",
-  "library.dae",
-  "library_label.dae",
-  "glow.png",
-]
-
-
-dataFiles      = ["../data/" + f for f in dataFiles]
-
-def songFiles(song, extra = []):
-  return ["../data/songs/%s/%s" % (song, f) for f in ["guitar.ogg", "notes.mid", "song.ini", "song.ogg"] + extra]
-
-dataFiles = [
-  ("data",                    dataFiles),
-  ("data/translations",       glob.glob("../data/translations/*.mo")),
-]
+import os
 
 if os.name == "nt":
-  setup(version = Version.version(),
-        description = "Rockin' it Oldskool!",
-        name = "Frets on Fire",
-        url = "http://www.unrealvoodoo.org",
-        windows = [
-          {
-            "script":          "FretsOnFire.py",
-            "icon_resources":  [(1, "fof.ico")]
-          }
-        ],
-        zipfile = "data/library.zip",
-        data_files = dataFiles,
-        options = options)
-else:
-  setup(version = Version.version(),
-        description = "Rockin' it Oldskool!",
-        name = "Frets on Fire",
-        url = "http://www.unrealvoodoo.org",
-        data_files = dataFiles,
-        options = options)
+  import setup_exe
+  setup_exe.setupWindows()
 
+#MFH - other OS users please fill out the following as you adapt this convention for your build scripts, using the below skeleton
+#elif os.name == "posix":
+
+#elif os.name == "mac":
+
+#elif os.name == "dos":
+  
+#elif os.name == "os2":
+
+#elif os.name == "ce":
