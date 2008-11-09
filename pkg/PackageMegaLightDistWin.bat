8852a86ad9efc917b8f488f7f04dc503f5062810
Chris Paiano <chris.paiano@gmail.com>
2008-11-09 07:07:30 +0000

    Added file exclusion list for Windows packaging scripts (to always prevent pesky Thumbs.db from being archived)
    git-svn-id: http://fofix.googlecode.com/svn/MFH-Mod/trunk@312 48f48816-a574-47fe-9a59-6914b3aef992

--- pkg/PackageMegaLightDistWin.bat
+++ pkg/PackageMegaLightDistWin.bat
@@ -1,6 +1,6 @@
 cd ..
 copy fretsonfire.ini fretsonfire.bak
 copy pkg\fretsonfire.fresh.ini fretsonfire.ini
-"C:\Program Files\WinRAR\winrar" u -dh -s -m5 -tl -ibck pkg\FoFiX-Full-Windows(MegaLight).rar @pkg\Dist-MegaLight.lst
+"C:\Program Files\WinRAR\winrar" u -dh -s -m5 -tl -ibck -x@pkg\filesToExclude.lst pkg\FoFiX-Full-Windows(MegaLight).rar @pkg\Dist-MegaLight.lst
 copy fretsonfire.bak fretsonfire.ini
 del fretsonfire.bak
