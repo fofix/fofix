bccab1703baa361f2b937b42023f14c97d1e2bb1
Chris Paiano <chris.paiano@gmail.com>
2009-03-21 16:16:49 +0000

    Updated RebuildWin script for compatibility with future game app name change from FretsOnFire to FoFiX, as well as any future library.zip-like archives that are implemented to clean up the data\ folder.
    git-svn-id: http://fofix.googlecode.com/svn/MFH-Mod/trunk@1224 48f48816-a574-47fe-9a59-6914b3aef992

--- src/scripts/RebuildWin.bat
+++ src/scripts/RebuildWin.bat
@@ -4,14 +4,17 @@ pause
 cd scripts
 call ClearCompiledPycs.bat
 cd ..
+del *.exe
+del *.dll
+del *.exe.log
 del data\*.pyd
 del data\*.dll
+del data\*.zip
 rem pause
-copy dist\FretsOnFire.exe FretsOnFire.exe
-copy dist\w9xpopen.exe w9xpopen.exe
-copy dist\data\library.zip data\library.zip
+copy dist\data\*.zip data\
 copy dist\data\*.pyd data\
 copy dist\data\*.dll data\
+copy /y dist\*.exe
 copy /y dist\*.dll
 rd /s /q dist
 pause
