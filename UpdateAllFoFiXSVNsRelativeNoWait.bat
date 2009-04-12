7850deca3b02bcb5963a85493c760cd087bd3ce6
Chris Paiano <chris.paiano@gmail.com>
2009-04-12 22:50:05 +0000

    Added two useful Windows TortoiseSVN relative auto-update scripts that I use to keep FoFiX, the Wiki, and two SVN themes updated with no hassle.
    git-svn-id: http://fofix.googlecode.com/svn/MFH-Mod/trunk@1306 48f48816-a574-47fe-9a59-6914b3aef992

---
+++ UpdateAllFoFiXSVNsRelativeNoWait.bat
@@ -0,0 +1,21 @@
+set CloseOnEnd=1
+set TortoiseSVN=C:\Progra~1\TortoiseSVN\bin\
+set localpath=.\
+
+START /WAIT %TortoiseSVN%TortoiseProc.exe /command:update /path:"%localpath%" /closeonend:%CloseOnEnd%
+
+cd wiki
+START /WAIT %TortoiseSVN%TortoiseProc.exe /command:update /path:"%localpath%" /closeonend:%CloseOnEnd%
+
+cd ..\data\themes\Rock Band 1
+START /WAIT %TortoiseSVN%TortoiseProc.exe /command:update /path:"%localpath%" /closeonend:%CloseOnEnd%
+
+cd ..
+cd Rock Band 2
+START /WAIT %TortoiseSVN%TortoiseProc.exe /command:update /path:"%localpath%" /closeonend:%CloseOnEnd%
+
+cd ..
+cd ..
+cd ..
+rem pause
+
