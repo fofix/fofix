d575794d75fe7182e9edf17bbc69b564fea0c796
Pascal Giard <evilynux@gmail.com>
2009-04-16 19:35:30 +0000

    Drawing methods comparison. You'll here find a simple example of: 0: Vertex Buffer Object drawing 1: Array-based drawing 2: Direct-mode drawing
    Use left/right arrow keys to switch between the modes.
    
    
    git-svn-id: http://fofix.googlecode.com/svn/MFH-Mod/trunk@1331 48f48816-a574-47fe-9a59-6914b3aef992

---
+++ src/DrawingTest.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# Drawing methods comparison for FoFiX by Pascal Giard <evilynux@gmail.com>
+from OpenGL.GL import *
+from OpenGL.arrays import vbo
+from numpy import array, float32
+import pygame
+from pygame.locals import *
+
+rtri = 0.0
+mode = 0
+
+def init():
+    global mode, triangVbo, triangVtx
+    triangVbo = vbo.VBO(
+        array([[ 0,  1, 0],
+               [-1, -1, 0],
+               [ 1, -1, 0]],dtype=float32) )
+    triangVtx = array(
+        [[ 0,  1, 0],
+         [-1, -1, 0],
+         [ 1, -1, 0]], dtype=float32)
+
+def draw():
+    global mode, triangVbo, triangVtx, rtri
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    glPushMatrix()
+    # draw triangle
+    glScale(.5,.5,.5)
+    glRotate(rtri,0,1,0)
+    # VBO drawing
+    if mode == 0:
+      triangVbo.bind()
+      glEnableClientState(GL_VERTEX_ARRAY);
+      glVertexPointerf( triangVbo )
+      glDrawArrays(GL_TRIANGLES, 0, 3)
+      glDisableClientState(GL_VERTEX_ARRAY)
+      triangVbo.unbind()
+    # Array-based
+    elif mode == 1:
+      glEnableClientState(GL_VERTEX_ARRAY)
+      glVertexPointerf(triangVtx)
+      glDrawArrays(GL_TRIANGLES, 0, 3)
+      glDisableClientState(GL_VERTEX_ARRAY)
+    # Direct drawing
+    else: # mode == 2
+      glBegin(GL_TRIANGLES)
+      glVertex( 0, 1,0)
+      glVertex(-1,-1,0)
+      glVertex( 1,-1,0)
+      glEnd()
+    glPopMatrix()
+
+def main():
+    global rtri, mode
+    fps = 0
+    video_flags = DOUBLEBUF|OPENGL|HWPALETTE|HWSURFACE
+    
+    pygame.init()
+    pygame.display.set_mode((640,480), video_flags)
+    init()
+
+    frames = 0
+    ticks = pygame.time.get_ticks()
+    clock = pygame.time.Clock()
+    while 1:
+      event = pygame.event.poll()
+      if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+        break
+      elif event.type == KEYDOWN and (event.key == K_RIGHT or event.key == K_LEFT):
+        mode = (mode + 1) % 3
+        
+      ticksDiff = pygame.time.get_ticks()-ticks
+
+      # draw all objects
+      draw()
+        
+      # update rotation counters
+      rtri += 0.2
+
+      # make changes visible
+      pygame.display.flip()
+
+      frames = frames+1
+      if( ticksDiff > 2000 ):
+        fps = ((frames*1000)/(ticksDiff))
+        ticks = pygame.time.get_ticks()
+        frames = 0
+        print "mode: %s, %.2f fps" % (mode, fps)
+      # evilynux - commented the following so we go as fast as we can
+      #clock.tick(60)
+
+if __name__ == '__main__': main()
+
