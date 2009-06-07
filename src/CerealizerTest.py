fofix/fofix

commit 37a117f0dd43f547755b2ad33a3669bcaf13ede9

Really fixed issue 691.
git-svn-id: http://fofix.googlecode.com/svn/MFH-Mod/trunk@1609 48f48816-a574-47fe-9a59-6914b3aef992

src/GameResultsScene.py
@@ -542,8 +542,8 @@ class GameResultsSceneClient(GameResultsScene, SceneClient):
       scores     = {}
       scores_ext = {}
       upname = self.playerList[i].name
-      scoreHash = hashlib.sha1("%d%d%d%s" % (player.getDifficultyInt(), self.finalScore[i], self.scoring[i].stars, self.playerList[i].name)).hexdigest()
-      scores[player.getDifficultyInt()]     = [(self.finalScore[i], self.scoring[i].stars, self.playerList[i].name, scoreHash)]
+      scoreHash = hashlib.sha1("%d%d%d%s" % (player.getDifficultyInt(), self.finalScore[i], self.scoring[i].stars, self.playerList[i].upname)).hexdigest()
+      scores[player.getDifficultyInt()]     = [(self.finalScore[i], self.scoring[i].stars, self.playerList[i].upname, scoreHash)]
       scores_ext[player.getDifficultyInt()] = [(scoreHash, self.scoring[i].stars) + scoreExt]
       d["scores"] = binascii.hexlify(Cerealizer.dumps(scores))
       d["scores_ext"] = binascii.hexlify(Cerealizer.dumps(scores_ext))
@@ -1354,7 +1354,7 @@ class GameResultsSceneClient(GameResultsScene, SceneClient):
         Theme.setBaseColor(1 - v)
         if self.uploadResponse[j] is None:
           upScoreText = _("Uploading Scores...")
-          font.render("P%d (%s) %s" % (j+1, player.name, upScoreText), (.05, sYPos + v), scale = sScale)
+          font.render("P%d (%s) %s" % (j+1, player.upname, upScoreText), (.05, sYPos + v), scale = sScale)
         else:
           result = str(self.uploadResponse[j]).split(";")
           if len(result) > 0:

commit a0fa51127f1beaf49c3e08c1ede7c7b70941eb2d

Unit test attempting to validate Cerealizer functionality. That was also an effort toward debugging score uploading bug introduced in r1609. Unfortunatly, everything looks good here.
Note that in the second test, the print shows that the name string is UTF-8 encoded. Could that be the issue or are all strings sent UTF-8 (e.g. urllib.urlencode doing the utf8 conversion)?



git-svn-id: http://fofix.googlecode.com/svn/MFH-Mod/trunk@1633 48f48816-a574-47fe-9a59-6914b3aef992

src/CerealizerTest.py
@@ -1,0 +1,60 @@
+#####################################################################
+# -*- coding: iso-8859-1 -*-                                        #
+#                                                                   #
+# FoFiX                                                             #
+# Copyright (C) 2009 Pascal Giard <evilynux@gmail.com>              #
+#                                                                   #
+# This program is free software; you can redistribute it and/or     #
+# modify it under the terms of the GNU General Public License       #
+# as published by the Free Software Foundation; either version 2    #
+# of the License, or (at your option) any later version.            #
+#                                                                   #
+# This program is distributed in the hope that it will be useful,   #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of    #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     #
+# GNU General Public License for more details.                      #
+#                                                                   #
+# You should have received a copy of the GNU General Public License #
+# along with this program; if not, write to the Free Software       #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,        #
+# MA  02110-1301, USA.                                              #
+#####################################################################
+import unittest
+try:
+  import hashlib
+except ImportError:
+  import sha
+  class hashlib:
+    sha1 = sha.sha
+import Cerealizer
+import binascii
+import urllib
+
+class CerealizerTest(unittest.TestCase):
+  def setUp(self):
+    difficulty = 0      # Expert
+    score = 123456      # Final score
+    nbrStars = 4        # Number of stars
+    name = "SomePlayer" # Player name
+    scoreHash = hashlib.sha1("%d%d%d%s" % (difficulty, score, nbrStars, name)
+                             ).hexdigest()
+    self.scores = {}
+    self.scores[difficulty] = [(score, nbrStars, name, scoreHash)]
+
+  def testIntegrity(self):
+    expected = self.scores
+    scoresSerial = binascii.hexlify(Cerealizer.dumps(self.scores))
+    result = Cerealizer.loads(binascii.unhexlify(scoresSerial))
+    self.assertEqual(result, expected)
+
+  def testHash(self):
+    scoresHash = "63657265616c310a330a646963740a6c6973740a7475706c650a340a"\
+                 "6936333834360a69350a7531310a50617363616c4769617264733430"\
+                 "0a343839666538656632343239646564373637363835373930303936"\
+                 "31323531633136303662373863310a72310a69310a310a72320a72300a"
+    scores = Cerealizer.loads(binascii.unhexlify(scoresHash))
+    #print scores[1][0][2]
+    self.assertEqual(scores[1][0][2], "PascalGiard")
+
+if __name__ == "__main__":
+  unittest.main()