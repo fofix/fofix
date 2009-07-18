50d7b8df66bf176048410c19267672017e2a472b
Pascal Giard <evilynux@gmail.com>
2009-07-18 00:45:14 +0000

    2 more testcases for score uploading. I had forgotten to commit it after working on issue 691.
    git-svn-id: http://fofix.googlecode.com/svn/MFH-Mod/trunk@1771 48f48816-a574-47fe-9a59-6914b3aef992

--- src/CerealizerTest.py
+++ src/CerealizerTest.py
@@ -48,7 +48,8 @@ class CerealizerTest(unittest.TestCase):
     self.assertEqual(result, expected)
 
   def testHash(self):
-    # For the record, the worldchart rejects those hashes:
+    # For the record, the worldchart rejects the following 3 hashes...
+    # (name strings are UTF8 encoded)
     scoresHash = "63657265616c310a330a646963740a6c6973740a7475706c650a340a"\
                  "6936333834360a69350a7531310a50617363616c4769617264733430"\
                  "0a343839666538656632343239646564373637363835373930303936"\
@@ -56,7 +57,6 @@ class CerealizerTest(unittest.TestCase):
     scores = Cerealizer.loads(binascii.unhexlify(scoresHash))
     #print scores[1][0][2]
     self.assertEqual(scores[1][0][2], "PascalGiard")
-
     #scoreExtHash = "63657265616c310a330a646963740a6c6973740a7475706c650a"\
     #               "390a7334300a3438396665386566323432396465643736373638"\
     #               "353739303039363132353163313630366237386369350a693236"\
@@ -64,6 +64,27 @@ class CerealizerTest(unittest.TestCase):
     #               "69300a73300a6936333834360a310a72310a69310a310a72320a"\
     #               "72300a"
     #songHash = "6551c0f99efddfd3c5c7ef2d407c81b8e3001a43"
+
+    # Worldchart accepts this one (name string is NOT UTF8 encoded)
+    scoresHash = "63657265616c310a330a646963740a6c6973740a7475706c650a340a"\
+                 "693234323032320a69350a73350a417a7a636f7334300a3233336431"\
+                 "37373139653539323066366461373034633432343864646632313930"\
+                 "32323939656438310a72310a69300a310a72320a72300a"
+    scores = Cerealizer.loads(binascii.unhexlify(scoresHash))
+    self.assertEqual(scores[0][0][2], "Azzco")
+    #print scores[0][0] # Shows that name string IS NOT UTF8 encoded
+
+    # Worldchart rejects this one (name string is UTF8 encoded)
+    scoresHash = "63657265616c310a330a646963740a6c6973740a7475706c650a340a"\
+                 "693135313632390a69340a75350a417a7a636f7334300a3661306562"\
+                 "35346438343962613065376464363836396430373966386631316366"\
+                 "61333133633264310a72310a69300a310a72320a72300a"
+    scores = Cerealizer.loads(binascii.unhexlify(scoresHash))
+    self.assertEqual(scores[0][0][2], "Azzco")
+    print scores[0][0] # Shows that name string IS UTF8 encoded
+    print scores[0][0][2]
+    score = scores[0][0][0], scores[0][0][1], str(scores[0][0][2]), scores[0][0][3]
+    print score
 
 if __name__ == "__main__":
   unittest.main()
