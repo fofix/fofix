fcbdde8dcb315d09a9d58ca5aebd1a6fd743f602
Pascal Giard <evilynux@gmail.com>
2009-08-20 14:35:28 +0000

    - Use decodebin2 as decodebin is depricated. - Replaced hackish workaround to detect EOS with a cleaner solution (thanks max26199).
    git-svn-id: http://fofix.googlecode.com/svn/MFH-Mod/trunk@1811 48f48816-a574-47fe-9a59-6914b3aef992

--- src/VideoPlayer.py
+++ src/VideoPlayer.py
@@ -142,7 +142,7 @@ class VideoPlayer(BackgroundLayer):
     with_audio = ""
     if not self.mute:
       with_audio = "! queue ! audioconvert ! audiorate ! audioresample ! autoaudiosink"
-    s = "filesrc name=input ! decodebin name=dbin dbin. ! ffmpegcolorspace ! video/x-raw-rgb ! fakesink name=output signal-handoffs=true sync=true dbin. %s" % with_audio
+    s = "filesrc name=input ! decodebin2 name=dbin dbin. ! ffmpegcolorspace ! video/x-raw-rgb ! fakesink name=output signal-handoffs=true sync=true dbin. %s" % with_audio
     self.player = gst.parse_launch(s)
     self.input  = self.player.get_by_name('input')
     self.fakeSink = self.player.get_by_name('output')
@@ -152,26 +152,38 @@ class VideoPlayer(BackgroundLayer):
     # FIXME: Doesn't work!?! The event is never received
     # Ok, messages are sent if i use the following in run():
     #  gobject.MainLoop().get_context().iteration(True)
-    # BUT the main python thread then freezes after ~5 seconds... why?!
-    # See run() for the current hackish workaround.
-#     bus = self.player.get_bus()
-#     bus.add_signal_watch()
-#     bus.enable_sync_message_emission()
-#     bus.connect("message", self.onMessage)
+    # BUT the main python thread then freezes after ~5 seconds...
+    # unless we use gobject.idle_add(self.player.elements)
+    # See run() for another hackish workaround.
+    gobject.idle_add(self.player.elements)
+    bus = self.player.get_bus()
+    bus.add_signal_watch()
+    bus.enable_sync_message_emission()
+    bus.connect("message", self.onMessage)
 #     bus.connect("message::eos", self.onEndOfStream)
+    # Required to prevent the main python thread from freezing, why?!
+    # Thanks to max26199 for finding this!
+    gobject.idle_add(self.player.elements)
 
   # Handle end of video
-  def onEndOfStream(self):
-    print "The END"
+#   def onEndOfStream(self, bus, message):
+#     print "The END"
 
   # Handle bus event e.g. end of video or unsupported formats/codecs
   def onMessage(self, bus, message):
     type = message.type
-    print "Message %s" % type
+#     print "Message %s" % type
     if type == gst.MESSAGE_EOS:
-      print "End of video"
-      self.player.set_state(gst.STATE_NULL)
-      self.finished = True
+#       print "End of video"
+      if self.loop:
+        self.player.set_state(gst.STATE_NULL)
+        # HACKISH: Need to recreate the pipepile altogether...
+        # For some reason going through STATE_NULL, STATE_READY, STATE_PLAYING
+        # doesn't work as I would expect.
+        self.videoSetup()
+      else:
+        self.player.set_state(gst.STATE_NULL)
+        self.finished = True
     elif type == gst.MESSAGE_ERROR:
       err, debug = message.parse_error()
       Log.error("Error: %s" % err, debug)
@@ -214,18 +226,19 @@ class VideoPlayer(BackgroundLayer):
       self.finished = False
     # HACKISH: The following is a freakin' ugly hack to workaround the gst
     # threading issue (see comments at the bottom of videoSetup).
-    # It'd be nice to get this working properly i.e. using onMessage.
-    s = self.fakeSink.get_property("last-message")
-    if s and s.find("type: 86") != -1: # 86 means EndOfStream (EOS)
-      if self.loop:
-        self.player.set_state(gst.STATE_NULL)
-        # HACKISH: Need to recreate the pipepile altogether...
-        # For some reason going through STATE_NULL, STATE_READY, STATE_PLAYING
-        # doesn't work as I would expect.
-        self.videoSetup()
-      else:
-        self.player.set_state(gst.STATE_NULL)
-        self.finished = True
+    # Thanks to max26199, this is no longer necessary
+#     s = self.fakeSink.get_property("last-message")
+#     if s and s.find("type: 86") != -1: # 86 means EndOfStream (EOS)
+#       if self.loop:
+#         self.player.set_state(gst.STATE_NULL)
+#         # HACKISH: Need to recreate the pipepile altogether...
+#         # For some reason going through STATE_NULL, STATE_READY, STATE_PLAYING
+#         # doesn't work as I would expect.
+#         self.videoSetup()
+#       else:
+#         self.player.set_state(gst.STATE_NULL)
+#         self.finished = True
+    gobject.MainLoop().get_context().iteration(True)
     self.clock.tick(self.fps)
     
   # Render texture to polygon
