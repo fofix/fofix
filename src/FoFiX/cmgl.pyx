813ff6f1228cf5e6de13e3082c509a8ff34303a0
John Stumpo <stump@jstump.com>
2010-02-28 05:51:57 -0500

    Add a wrapper around glDrawArrays to cmgl.

--- src/FoFiX/cmgl.pyx
+++ src/FoFiX/cmgl.pyx
@@ -22,13 +22,24 @@
 # (that is, support for Python 2.5+'s "with" statement) wherever it
 # makes sense, and even some places where it doesn't...
 
+import numpy
+cimport numpy
+
 cdef extern from "GL/gl.h":
+  ctypedef int GLint
   ctypedef unsigned int GLuint
   ctypedef unsigned int GLsizei
   ctypedef int GLenum
+  ctypedef float GLfloat
+  ctypedef void GLvoid
 
   enum:
     GL_COMPILE
+    GL_FLOAT
+    GL_VERTEX_ARRAY
+    GL_COLOR_ARRAY
+    GL_TEXTURE_COORD_ARRAY
+    GL_NORMAL_ARRAY
 
   void glPushMatrix()
   void glPopMatrix()
@@ -38,6 +49,14 @@ cdef extern from "GL/gl.h":
   void glNewList(GLuint, GLenum)
   void glEndList()
   void glCallList(GLuint)
+
+  void glVertexPointer(GLint, GLenum, GLsizei, GLvoid*)
+  void glColorPointer(GLint, GLenum, GLsizei, GLvoid*)
+  void glTexCoordPointer(GLint, GLenum, GLsizei, GLvoid*)
+  void glNormalPointer(GLenum, GLsizei, GLvoid*)
+  void glEnableClientState(GLenum)
+  void glDisableClientState(GLenum)
+  void glDrawArrays(GLenum, GLint, GLsizei)
 
 ## Context manager for a matrix push.
 # Inside its context, the active matrix is pushed.
@@ -68,3 +87,40 @@ cdef class cmglList(object):
 
   def __call__(self):
     glCallList(self.gl_list)
+
+## Wrapper for drawing from an arbitrary set of numpy arrays.
+def cmglDrawArrays(GLenum mode,
+                   numpy.ndarray[numpy.float32_t, ndim=2] vertices,
+                   numpy.ndarray[numpy.float32_t, ndim=2] colors=None,
+                   numpy.ndarray[numpy.float32_t, ndim=2] texcoords=None,
+                   numpy.ndarray[numpy.float32_t, ndim=2] normals=None):
+  try:
+    glEnableClientState(GL_VERTEX_ARRAY)
+    glVertexPointer(vertices.shape[1], GL_FLOAT, 0, vertices.data)
+    if colors is not None:
+      if colors.shape[0] != vertices.shape[0]:
+        raise TypeError, 'colors and vertices must be the same length'
+      glEnableClientState(GL_COLOR_ARRAY)
+      glColorPointer(colors.shape[1], GL_FLOAT, 0, colors.data)
+    if texcoords is not None:
+      if texcoords.shape[0] != vertices.shape[0]:
+        raise TypeError, 'texcoords and vertices must be the same length'
+      glEnableClientState(GL_TEXTURE_COORD_ARRAY)
+      glTexCoordPointer(texcoords.shape[1], GL_FLOAT, 0, texcoords.data)
+    if normals is not None:
+      if normals.shape[0] != vertices.shape[0]:
+        raise TypeError, 'normals and vertices must be the same length'
+      if normals.shape[1] != 3:
+        raise TypeError, 'normal vectors must have exactly 3 components'
+      glEnableClientState(GL_NORMAL_ARRAY)
+      glNormalPointer(GL_FLOAT, 0, texcoords.data)
+    glDrawArrays(mode, 0, vertices.shape[0])
+  finally:
+    glNormalPointer(GL_FLOAT, 0, NULL)
+    glTexCoordPointer(4, GL_FLOAT, 0, NULL)
+    glColorPointer(4, GL_FLOAT, 0, NULL)
+    glVertexPointer(4, GL_FLOAT, 0, NULL)
+    glDisableClientState(GL_NORMAL_ARRAY)
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY)
+    glDisableClientState(GL_COLOR_ARRAY)
+    glDisableClientState(GL_VERTEX_ARRAY)
