066d81d23bf735426e02f6b3c810143c7edc4f9b
nhydock <n_hydock@comcast.net>
2010-08-22 10:46:35 -0400

    Theme should force itself to Rockband theme until we get rid of the self.theme system all together.

--- src/Data.py
+++ src/Data.py
@@ -108,16 +108,9 @@ class Data(object):
     else:
       self.vocalPath = os.path.join("themes",themename,"vocals")
 
-    if self.checkImgDrawing(os.path.join("themes",themename,"spfill.png")):
-      self.theme = 0
-    elif self.checkImgDrawing(os.path.join("themes",themename,"overdrive fill.png")):
-      self.theme = 2
-      self.themeCoOp = True
-    else:
-      self.theme = 1
-      if self.checkImgDrawing(os.path.join("themes",themename,"coop_rockmeter.png")):
-        self.themeCoOp = True
-
+    self.theme = 2
+    self.themeCoOp = True
+    
     self.fontScreenBottom = 0.75      #from our current viewport's constant 3:4 aspect ratio (which is always stretched to fill the video resolution)
 
     self.loadPartImages()
