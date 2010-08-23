2bf97821d78f971b32598c1da77391ef0336e86e
nhydock <n_hydock@comcast.net>
2010-08-23 10:48:06 -0400

    Variable typo (forgot the .self)

--- src/Neck.py
+++ src/Neck.py
@@ -213,7 +213,7 @@ class Neck:
 
     def loadImage(name, file):
       if self.extension:
-        if not engine.loadImgDrawing(self, name, os.path.join(themepath, extension, file)):
+        if not engine.loadImgDrawing(self, name, os.path.join(themepath, self.extension, file)):
           engine.loadImgDrawing(self, name, os.path.join(themepath, file))
       else:
         engine.loadImgDrawing(self, name, os.path.join(themepath, file))
