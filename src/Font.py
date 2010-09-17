7c6ba3e436244acffd6165f3407d5c142619a688
fuzion <fuzion@nukeit.org>
2010-09-16 19:37:38 -0500

    Minor with_statement fixes for Python 2.5 compatibility.

--- src/Font.py
+++ src/Font.py
@@ -20,7 +20,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,        #
 # MA  02110-1301, USA.                                              #
 #####################################################################
-
+from __future__ import with_statement
 import pygame
 from OpenGL.GL import *
 from cmgl import *
--- src/Mesh.py
+++ src/Mesh.py
@@ -19,7 +19,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,        #
 # MA  02110-1301, USA.                                              #
 #####################################################################
-
+from __future__ import with_statement
 from OpenGL.GL import *
 
 import Collada
