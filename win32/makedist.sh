c1add013b4be98283ae873ccd559c89dcb1e9ee5
John Stumpo <stump@jstump.com>
2011-04-03 03:08:38 -0400

    deppack: Remove more unnecessary files and symbols in makedist.

--- win32/makedist.sh
+++ win32/makedist.sh
@@ -3,9 +3,10 @@
 mkdir -pv dist
 cp -av deps dist
 
-rm -rf dist/deps/build-stamps dist/deps/etc dist/deps/share dist/deps/lib/gettext
+rm -rf dist/deps/build-stamps dist/deps/doc dist/deps/etc dist/deps/share dist/deps/lib/gettext
 rm -vf dist/deps/lib/*.la dist/deps/bin/wine-shwrap dist/deps/bin/pkg-config
 i686-w64-mingw32-strip --strip-all dist/deps/bin/*.exe dist/deps/bin/*.dll
+i686-w64-mingw32-strip --strip-debug dist/deps/lib/*.lib dist/deps/lib/*.a
 ZIPFILE="fofix-win32-deppack-`date +%Y%m%d`.zip"
 rm -f "$ZIPFILE"
 (cd dist && zip -9r ../"$ZIPFILE" deps)
