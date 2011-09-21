9b9985c18a9aefccbc9fb7bc45d9d9fb1240d6fb
nhydock <n_hydock@comcast.net>
2011-09-21 17:07:41 -0400

    MLv4 Face-off rockmeter (experimental)

--- data/themes/MegaLight V4/CustomRMLayers.py
+++ data/themes/MegaLight V4/CustomRMLayers.py
@@ -1,38 +1,52 @@
 
 from Rockmeter import *
 
-class Spinner(ImageLayer):
-    def __init__(self, stage, section):
+class CoOpMeter(Group):
+  def __init__(self, stage, section):
     
-        self.stage = stage
-        super(Spinner, self).__init__(stage, section, os.path.join("themes", self.stage.themename, "rockmeter", "platinum.png"))
-        
-        self.xposexpr    = ".80"
-        self.yposexpr    = ".80"
-        
-        self.condition = "True"
-        
-    def updateLayer(self, playerNum):
-        super(ImageLayer, self).updateLayer(playerNum)
-        
-        self.angle += 2
-        
-    def render(self, visibility, playerNum):
-        
-        self.updateLayer(playerNum)
-        
-        coord      = self.position
-        scale      = self.scale
-        rot        = self.angle
-        color      = self.color
-        alignment  = self.alignment
-        valignment = self.valignment
-        drawing    = self.drawing
-        rect       = self.rect
+    super(CoOpMeter, self).__init__(stage, section)
     
-        #frameX  = self.frameX
-        #frameY  = self.frameY
-
-        if bool(eval(self.condition)):
-            self.engine.drawImage(drawing, scale, coord, rot, color, rect, 
-                            alignment = alignment, valignment = valignment)        
+    self.ySpacingExpr = self.getexpr("yspacing", "0.0")
+    self.ySpacing = 0.0
+    self.condition = "True"
+        
+    print self.layers.values()
+    
+  def updateLayer(self, playerNum):
+    super(CoOpMeter, self).updateLayer(playerNum)
+    
+    self.ySpacing = eval(self.ySpacingExpr)
+    
+  def render(self, visibility, playerNum):
+    w, h, = self.engine.view.geometry[2:4]
+   
+    self.updateLayer(playerNum)
+    for effect in self.effects:
+      effect.update()
+    
+    self.engine.view.setViewportHalf(1,0)
+    i = playerNum
+    scale = (.75*(len(self.stage.scene.playerList)/2))
+      
+    for layer in self.layers.values():
+      layer.updateLayer(playerNum)
+      for effect in layer.effects:
+        effect.update()
+      layer.position = [p*scale for p in layer.position]
+      layer.scale = [s*scale for s in layer.scale]
+      if isinstance(layer, FontLayer):
+        layer.position = [layer.position[0] + (self.position[0]/w), layer.position[1] - (self.position[1]/h)*.75]
+        layer.position[1] -= (self.ySpacing*scale/h)*.75*(i/2)
+      else:
+        layer.position = [layer.position[n] + self.position[n] for n in range(2)]
+        layer.position[1] -= (self.ySpacing*scale/h)*(i/2)
+        
+      #flips across screen for every other player
+      if i % 2 == 1:
+        layer.position[0] = w - layer.position[0]
+        layer.scale[0] *= -1
+        layer.angle *= -1
+    
+      #layer.scale = [s/(2.0*(len(self.stage.scene.playerList)/2)) for s in layer.scale]
+      layer.render(visibility, playerNum)
+      
