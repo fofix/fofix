f8c6d5d35e7c835f4006d53b3be162e7c8aebc5c
John Stumpo <stump@jstump.com>
2012-07-19 02:09:35 -0400

    deppack: Make makedist a little more verbose.

--- win32/makedist.sh
+++ win32/makedist.sh
@@ -1,12 +1,19 @@
 #!/bin/sh -e
 
-mkdir -pv dist
-cp -av deps dist
+echo -n "Copying deps/ into dist/... "
+mkdir -p dist
+cp -a deps dist
+echo "done"
 
+echo -n 'Removing unnecessary files... '
 rm -rf dist/deps/build-stamps dist/deps/doc dist/deps/etc dist/deps/share dist/deps/lib/gettext
-rm -vf dist/deps/lib/*.la dist/deps/bin/wine-shwrap dist/deps/bin/pkg-config
+rm -f dist/deps/lib/*.la dist/deps/bin/wine-shwrap dist/deps/bin/pkg-config
+echo "done"
+
+echo -n "Stripping binaries and libraries... "
 i686-w64-mingw32-strip --strip-all dist/deps/bin/*.exe dist/deps/bin/*.dll
 i686-w64-mingw32-strip --strip-debug dist/deps/lib/*.lib dist/deps/lib/*.a
+echo "done"
 ZIPFILE="fofix-win32-deppack-`date +%Y%m%d`.zip"
 rm -f "$ZIPFILE"
 (cd dist && zip -9r ../"$ZIPFILE" deps)
