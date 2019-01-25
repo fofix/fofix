b5d5f3aeef65846e42c485bf51e097c550651f60
shikulja <shikulja@rambler.ru>
2019-01-25 23:20:02 +0300

    change buildPackageWin.cmd
    for normal build package
    del svn wiki
    del cd src folder

--- pkg/buildPackageWin.cmd
+++ pkg/buildPackageWin.cmd
@@ -1,18 +1,16 @@
 REM  this script will build the binarys with py2exe
 REM  and makes the folder ready for distribution
 
-cd ..\src
+cd ..
 python setup.py py2exe
 
 rd /S /Q build
 rd /S /Q scripts
 del *.pyc
 
-cd ..
+
 xcopy dist\* . /E /Y
 rd /S /Q dist
 rd /S /Q svg
-
-svn export http://fofix.googlecode.com/svn/wiki wiki
 
 pause
