a4613612c91820aa35f9716feaee804434af894d
François Magimel <magimel.francois@gmail.com>
2021-04-18 16:23:40 +0200

    refactor(mesh): remove useless code
    The `_unflatten` method is not used anymore.

--- fofix/tests/core/test_mesh.py
+++ fofix/tests/core/test_mesh.py
@@ -12,14 +12,6 @@ class MeshTest(unittest.TestCase):
     def setUp(self):
         self.filename = os.path.join('data', 'note.dae')
 
-    def test_unflatten(self):
-        """ Test the _unflatten method """
-        mesh = Mesh(self.filename)
-        vertices = range(6)
-        stride = 2
-        unflat = mesh._unflatten(vertices, stride)
-        self.assertEqual(unflat, [(0, 1), (2, 3), (4, 5)])
-
     def test_find(self):
         mesh = Mesh(self.filename)
         mesh.render()
