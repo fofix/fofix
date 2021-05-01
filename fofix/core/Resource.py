ad4988a2550dc14c5a111b10d0230b780d92adf7
François Magimel <magimel.francois@gmail.com>
2021-05-01 20:36:10 +0200

    refactor(resource): raise future-proof exceptions

--- fofix/core/Resource.py
+++ fofix/core/Resource.py
@@ -133,7 +133,10 @@ class Loader(Thread):
             log.info("Loaded %s.%s in %.3f seconds" % (self.target.__class__.__name__, self.name, self.time))
 
         if self.exception:
-            raise self.exception[0], self.exception[1], self.exception[2]
+            e = self.exception[0](self.exception[1])
+            e.__traceback__ = self.exception[2]
+            raise e
+            # raise e.with_traceback(self.exception[2])
         if self.target and self.name:
             setattr(self.target, self.name, self.result)
         if self.onLoad:
